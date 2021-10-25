c8b576131270cefc44dc8b43f2ef84d7a09441e1
steevefreedy <steevefreedy@gmail.com>
2021-10-25 14:37:36 +0100

    mise

--- view/calendar.php
+++ view/calendar.php
@@ -22,46 +22,19 @@
           <div class="row">
             <div class="col-md-6" >
               <div class="card" style="width: 18rem;">
-                  <img src="/assets/images/3.jpeg" class="card-img-top" alt="...">
+                  <img src="/assets/images/c1.png" class="card-img-top" alt="...">
                   <div class="card-body">
-                    <h5 class="card-title">Projet dernièrement ouvert</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
                   </div>
               </div>
             </div>
             <div class="col-md-6" >
               <div class="card" style="width: 18rem;">
-                  <img src="/assets/images/3.jpeg" class="card-img-top" alt="...">
+                  <img src="/assets/images/c2.jpg" class="card-img-top" alt="...">
                   <div class="card-body">
-                    <h5 class="card-title">Tâches prioritaires</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
                   </div>
               </div>
-            </div>
-            <hr>
-            <div class="row">
-            <div class="col-md-6" >
-              <div class="card" style="width: 18rem;">
-                  <img src="/assets/images/3.jpeg" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Projet dernièrement ouvert</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
-              </div>
-            </div>
-            <div class="col-md-6" >
-              <div class="card" style="width: 18rem;">
-                  <img src="/assets/images/3.jpeg" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Tâches prioritaires</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
-              </div>
-            </div>
             </div>
           <?php  require_once ('composants/footer.php') ?>
         </div>
